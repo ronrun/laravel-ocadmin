bf9b3d50bf88a9104167ed555e0ce419add8737d
Ron Lee <arhatron@gmail.com>
2022-06-23 00:42:47 +0800

    Some fix

---
+++ app/Models/MemberIp.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MemberIp extends Model
+{
+    use HasFactory;
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('username',30);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('is_active')->default('1');
+            $table->boolean('status')->default('1');
             $table->rememberToken();
             $table->timestamps();
         });
